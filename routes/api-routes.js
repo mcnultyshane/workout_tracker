83f48afc514c6cef746d962e1d18bb42944a39c3
mcnultyshane <82954885+mcnultyshane@users.noreply.github.com>
2021-07-27 16:25:05 -0400

    added api routes.

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -6,11 +6,57 @@ const db = require("../models/exercise")
 
 
 // GET Request for getting all workouts
+router.get("/api/workouts", (req, res) => {
+    db.find()
+        .then((dbData) => {
+            res.json(dbData);
+        })
+        .catch((err) => {
+            res.json(err);
+        });
+});
 
 // GET request
+router.get("/api/workouts/range", (req, res) => {
+    db.find()
+        .then((dbData) => {
+            res.json(dbData);
+        })
+        .catch((err) => {
+            res.json(err);
+        });
+});
 
 // POST Workout
+router.post("/api/workouts", ({
+    body
+}, res) => {
+    db.create(body)
+        .then((dbData) => {
+            res.json(dbData);
+        })
+        .catch((err) => {
+            res.json(err);
+        });
+});
 
 // PUT/ Update Workout
+
+router.put("/api/workouts/:id", ({
+    body,
+    params
+}, res) => {
+    db.findByIdAndUpdate(params.id, {
+            $push: {
+                exercises: body
+            }
+        })
+        .then((dbData) => {
+            res.json(dbData);
+        })
+        .catch((err) => {
+            res.json(err);
+        });
+})
 
 module.exports = router
