7cbc75efca875a23c9677a0448a22bf17d9c3cea
mcnultyshane <82954885+mcnultyshane@users.noreply.github.com>
2021-07-27 16:12:34 -0400

    mapping api routes.

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -3,3 +3,14 @@ const router = ("express").Router();
 
 // import workout model
 const db = require("../models/exercise")
+
+
+// GET Request for getting all workouts
+
+// GET request
+
+// POST Workout
+
+// PUT/ Update Workout
+
+module.exports = router
