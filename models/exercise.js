ae51491486c22df237213dfd0a5b1344b6357ea1
mcnultyshane <82954885+mcnultyshane@users.noreply.github.com>
2021-07-26 20:53:23 -0400

    rough workout of workout schema.

---
+++ models/exercise.js
@@ -0,0 +1,47 @@
+const mongoos = require('mongooose');
+
+// Mongoose Schema
+const Schema = mongoose.Schema;
+
+// New workout Schema
+const WorkoutSchema = new Schema({
+    day: {
+        type: Date,
+        default: Date.now()
+    },
+    exercises: [{
+        name: {
+            type: String,
+            trim: true,
+            required: "Please Enter Exercise Name"
+        },
+        type: {
+            type: String,
+            trim: true,
+            required: "Please Enter Exercise"
+        },
+        distance: {
+            type: Number
+        },
+        duration: {
+            type: Number,
+            required: "Please enter the duration of the exercise."
+        },
+        weight: {
+            type: Number
+        },
+        sets: {
+            type: Number
+        },
+        reps: {
+            type: Number
+        },
+    }]
+});
+
+// create mongoose model 'workout and apply workout schema to that model
+
+const Workout = mongoose.model('workout', WorkoutSchema);
+
+// Export Workout Model
+module.exports = Workout;
