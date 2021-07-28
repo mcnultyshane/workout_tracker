d4344b6997e15583bc126848649783da2d337495
mcnultyshane <82954885+mcnultyshane@users.noreply.github.com>
2021-07-27 20:51:58 -0400

    deploying.

--- server.js
+++ server.js
@@ -17,7 +17,7 @@ app.use(express.urlencoded({
 app.use(express.json());
 
 app.use(express.static("public"));
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/fitnesstracker", {
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/fitness-tracker--s", {
     useNewUrlParser: true,
     useUnifiedTopology: true,
     useFindAndModify: false,
