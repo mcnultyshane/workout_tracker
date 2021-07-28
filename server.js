0a68516c24964cbd3bfb7fba7b8d5f2b73c6625d
mcnultyshane <82954885+mcnultyshane@users.noreply.github.com>
2021-07-27 20:46:36 -0400

    redirecting routes and troubleshooting seeder

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const PORT = process.env.PORT || 3000;
 // const db = require("./models");
 const app = express();
 
-// app.use(logger("dev"));
+app.use(logger("dev"));
 
 app.use(express.urlencoded({
     extended: true
